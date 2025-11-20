69be1d6f397b99aea854f5ddaab3fdd3bc8eb59b
Bxk756 <225787637+Bxk756@users.noreply.github.com>
2025-11-20 13:02:50 -0500

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="py-10 text-center opacity-70 text-sm">
+      Revolution Tomorrow™ — A Swarm Shield LLC Project  
+      <br/> © 2025 All Rights Reserved
+    </footer>
+  );
+}
